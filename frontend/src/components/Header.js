56a74f86dc204ab82890be9f8dce4205e44f0b7d
Namitha Reddy <namithareddy066@gmail.com>
2023-03-10 23:30:39 +0530

    Built HomeScreen and Product Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import {LinkContainer} from 'react-router-bootstrap'
+import {Link} from 'react-router-dom'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
     <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/">E-CART</Navbar.Brand>
+      <Link to='/'>
+      <Navbar.Brand>E-CART</Navbar.Brand>
+      </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/cart"><i className="fas fa-solid fa-cart-shopping"></i> Cart</Nav.Link>
-            <Nav.Link href="/login"><i className="fas fa-solid fa-user"></i> Sign In</Nav.Link>
+             <Link to='/cart'>
+            <Nav.Link ><i className="fas fa-solid fa-cart-shopping"></i> Cart</Nav.Link>
+             </Link>
+             <Link to='/login'>
+            <Nav.Link ><i className="fas fa-solid fa-user"></i> Sign In</Nav.Link>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
