f20e9d44d6ee65640e20057157bfafaf543e684f
Namitha Reddy <namithareddy066@gmail.com>
2023-03-28 23:15:15 +0530

    Implemented redux actions and reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,19 @@
-import { configureStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer,productDetailsReducer} from './reducers/productReducers'
 
-const reducer = combineReducers({})
+
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: productDetailsReducer
+
+})
+
+// const initialState = {}
+const middleware = [thunk]
+const store = configureStore({reducer,middleware}, composeWithDevTools() )
+
+export default store
 
